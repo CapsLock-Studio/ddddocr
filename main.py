3cb76c7e10a1ca11fc5a0d1ec97de8ceaa2950bb
Michael Kuan <michael34435@capslock.tw>
2023-12-22 12:22:01 +0800

    feat: add ddddocr and fastapi instance

---
+++ main.py
@@ -0,0 +1,22 @@
+import ddddocr
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    data: str
+
+
+@app.post('/')
+def main(body: Image):
+    ocr_answer: str = ''
+    ocr = ddddocr.DdddOcr(show_ad=False, beta=True)
+
+    while len(ocr_answer) != 4:
+        ocr_answer = ocr.classification(body.data)
+
+    return {
+      'msg': ocr_answer,
+    }
