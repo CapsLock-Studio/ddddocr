3783dd6220406d56cb33b26a976b8c7d2f4ce19d
Michael.K <michael34435@capslock.tw>
2023-12-22 12:42:52 +0800

    feat: add strip

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def main(body: Image):
     ocr_answer: str = ''
     ocr = ddddocr.DdddOcr(show_ad=False, beta=True)
 
-    while len(ocr_answer) != 4:
-        ocr_answer = ocr.classification(body.data)
+    while len(ocr_answer) < 4:
+        ocr_answer = ocr.classification(body.data).strip()
 
     return {
       'msg': ocr_answer,
